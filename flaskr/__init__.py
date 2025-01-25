4ca304e4aa9da41fd95cfd27b8dcdc5474bc09f7
David Knöbl <david@Mac.fritz.box>
2025-01-25 13:59:10 +0100

    initial flask project

---
+++ flaskr/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
