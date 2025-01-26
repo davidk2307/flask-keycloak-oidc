6fff7c1c4498176a06fad57e5ea94ccd53c84e5e
David Knöbl <david@Mac.fritz.box>
2025-01-26 14:43:05 +0100

    added pkce code challenge

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -46,15 +46,25 @@ def create_app_oauth(test_config=None):
 
     from authlib.integrations.flask_client import OAuth
     from dotenv import load_dotenv
+    import base64
+    import re
+    import hashlib
 
     load_dotenv()
 
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
+    code_verifier = base64.urlsafe_b64encode(os.urandom(40)).decode('utf-8')
+    code_verifier = re.sub('[^a-zA-Z0-9]+', '', code_verifier)
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8')
+    code_challenge = code_challenge.replace('=', '')
+
     app.config.update(
         SECRET_KEY='flask-keycloak-oidc',
         KEYCLOAK_CLIENT_ID=os.environ["KEYCLOAK_CLIENT_ID"],
+        KEYCLOAK_CODE_CHALLENGE_METHOD="S256",
         KEYCLOAK_CLIENT_SECRET=os.environ["KEYCLOAK_CLIENT_SECRET"],
     )
 
@@ -79,11 +89,11 @@ def create_app_oauth(test_config=None):
     @app.route('/login')
     def login():
         redirect_uri = url_for('authorize', _external=True)
-        return oauth.keycloak.authorize_redirect(redirect_uri)
+        return oauth.keycloak.authorize_redirect(redirect_uri, code_challenge=code_challenge, code_challenge_method="S256")
 
     @app.route('/authorize')
     def authorize():
-        token = oauth.keycloak.authorize_access_token()
+        token = oauth.keycloak.authorize_access_token(code_verifier=code_verifier)
         session['user'] = token['userinfo']
         return redirect('/')
 
