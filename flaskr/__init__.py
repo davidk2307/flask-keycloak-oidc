f83c20e50c9eacf08a1df5bebb543e25b0283b9c
David Knöbl <david@Mac.fritz.box>
2025-01-26 10:32:50 +0100

    simple first login to keycloak

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,14 +1,44 @@
 import os
-from flask import Flask
+import json
+from flask import Flask,session
+from flask_oidc import OpenIDConnect
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config.update(
+        TESTING=True,
+        SECRET_KEY='flask-keycloak-oidc',
+        OIDC_SCOPES=["openid", "email", "profile"],
+        # client_secrets.json file is put in the instance folder (not in repo) and needs to have the following strucutre:
+        # {
+        #     "web": {
+        #         "client_id": "habook",
+        #         "client_secret": "",
+        #         "auth_uri": "",
+        #         "token_uri": "",
+        #         "userinfo_uri": "",
+        #         "issuer": "",
+        #         "redirect_uris": [""]
+        #     }
+        # }
+        OIDC_CLIENT_SECRETS=os.path.join(app.instance_path, 'client_secrets.json'),
+    )
+
+    oidc = OpenIDConnect(app)
     
-    # a simple page that says hello
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    def index():
+        if oidc.user_loggedin:
+            return 'Welcome %s' % session["oidc_auth_profile"].get('email')
+        else:
+            return 'Not logged in'
+
+    @app.route('/login')
+    @oidc.require_login
+    def login():
+        return 'Welcome %s' % session["oidc_auth_profile"].get('email')
 
     return app
 
